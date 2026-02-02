7cd89ef8a0cb5d2b056ed9e2f9bc3cb21d4072ea
Ville Vikingsson <villevi@student.chalmers.se>
2026-02-02 13:08:38 +0100

    wp0: ex1

---
+++ week1/wp0/assignment/ex1/main.c
@@ -0,0 +1,13 @@
+// (C) Aisha Attar, Vijaykrishnan Gopalakrishnan, Ville Vikingsson, group: 25 (2026)
+// Work package 0
+// Exercise 1
+// Submission code: 569045 (provided by your TA-s)
+
+#include <stdio.h>  // Import standard i/o library
+
+// Main function, the entry-point of the program.
+// argc == argument count and argv == argument vector. 
+// argv is an array of pointers all pointing to the first character in some array of characters.
+int main (int argc, char* argv[]) {
+    printf("%s", "Hello World!\n"); // String token replaced with string
+}
